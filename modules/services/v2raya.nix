b650e81c70d52ab7e8cc099ee223643899dca89e
tsssni <dingyongyu2002@foxmail.com>
2024-04-07 22:25:18 +0800

    use v2raya binary

--- modules/services/v2raya.nix
+++ modules/services/v2raya.nix
@@ -4,6 +4,11 @@ with lib;
 
 let
   cfg = config.services.v2raya;
+  assetsDir = with pkgs; symlinkJoin {
+    name = "assets";
+    paths = [ v2ray-geoip v2ray-domain-list-community ];
+  };
+
 in
 
 {
@@ -22,6 +27,7 @@ in
         EnvironmentVariables = {
           V2RAYA_LOG_FILE = "/tmp/v2raya.log";
           V2RAYA_V2RAY_BIN = "${pkgs.v2ray}/bin/v2ray";
+          XDG_DATA_DIR = "${assetsDir}/share";
         };
         ProgramArguments = [ "${cfg.package}/bin/v2raya" "--lite" ];
         KeepAlive = true;
--- pkgs/v2raya/default.nix
+++ pkgs/v2raya/default.nix
@@ -1,99 +1,29 @@
 { lib
-, fetchFromGitHub
-, mkYarnPackage
-, buildGoModule
-, makeWrapper
-, v2ray
-, v2ray-geoip
-, v2ray-domain-list-community
-, symlinkJoin
-, fetchYarnDeps
+, stdenv
+, fetchurl
 }:
-let
+stdenv.mkDerivation rec {
   pname = "v2raya";
   version = "2.2.5.1";
-
-  src = fetchFromGitHub {
-    owner = "v2rayA";
-    repo = "v2rayA";
-    rev = "v${version}";
-    hash = "sha256-aicKjirUHNeCCxfW9aaPI+X5DTQ0RdZnCxIQRU+GdCM=";
-    postFetch = "sed -i -e 's/npmmirror/yarnpkg/g' $out/gui/yarn.lock";
-  };
-  guiSrc = "${src}/gui";
-
-  web = mkYarnPackage {
-    inherit pname version;
-
-    src = guiSrc;
-    packageJSON = ./package.json;
-
-    offlineCache = fetchYarnDeps {
-      yarnLock = "${guiSrc}/yarn.lock";
-      sha256 = "sha256-AZIYkW2u1l9IaDpR9xiKNpc0sGAarLKwHf5kGnzdpKw=";
-    };
-
-    buildPhase = ''
-      runHook preBuild
-      OUTPUT_DIR=$out yarn --offline build
-      runHook postBuild
-    '';
-
-    configurePhase = ''
-      runHook preConfigure
-      cp -r $node_modules node_modules
-      chmod +w node_modules
-      runHook postConfigure
-    '';
-
-    distPhase = "true";
-
-    dontInstall = true;
-    dontFixup = true;
+  src = fetchurl {
+    url = "https://github.com/v2rayA/v2rayA/releases/download/v2.2.5.1/v2raya_darwin_arm64_2.2.5.1";
+    sha256 = "sha256-xh1HFR19lO0M1LoXt/GT0nEvnEB1hNOYiH42rzp9hCE=";
+    executable = true;
   };
 
-  assetsDir = symlinkJoin {
-    name = "assets";
-    paths = [ v2ray-geoip v2ray-domain-list-community ];
-  };
+  phases = [ "installPhase" ];
 
-in
-buildGoModule {
-  inherit pname version;
-
-  src = "${src}/service";
-  vendorHash = "sha256-/4l13TbE1WEX1xYfyzEwygFsNtT6weoYDll4ejvCyIg=";
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X github.com/v2rayA/v2rayA/conf.Version=${version}"
-  ];
-
-  subPackages = [ "." ];
-
-  nativeBuildInputs = [ makeWrapper ];
-  preBuild = ''
-    cp -a ${web} server/router/web
-  '';
-
-  postInstall = ''
-    install -Dm 444 ${src}/install/universal/v2raya.desktop -t $out/share/applications
-    install -Dm 444 ${src}/install/universal/v2raya.png -t $out/share/icons/hicolor/512x512/apps
-    substituteInPlace $out/share/applications/v2raya.desktop \
-      --replace 'Icon=/usr/share/icons/hicolor/512x512/apps/v2raya.png' 'Icon=v2raya'
-
-    wrapProgram $out/bin/v2rayA \
-      --prefix PATH ":" "${lib.makeBinPath [ v2ray ]}" \
-      --prefix XDG_DATA_DIRS ":" ${assetsDir}/share \
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${src} $out/bin/v2raya
   '';
 
   meta = with lib; {
     description = "A Linux web GUI client of Project V which supports V2Ray, Xray, SS, SSR, Trojan and Pingtunnel";
     homepage = "https://github.com/v2rayA/v2rayA";
-    mainProgram = "v2rayA";
+    mainProgram = "v2raya";
     license = licenses.agpl3Only;
     platforms = platforms.darwin;
-    maintainers = with maintainers; [ tsssni ];
   };
 }
+
