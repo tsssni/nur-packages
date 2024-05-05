8e1a41870acfddd0d6cf1a6b193b879d3ea0c603
tsssni <dingyongyu2002@foxmail.com>
2024-05-05 18:58:40 +0800

    update chrome version

--- pkgs/wired/google-chrome/default.nix
+++ pkgs/wired/google-chrome/default.nix
@@ -5,12 +5,12 @@
 let
   google-chrome = fetchurl {
     url = "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg";
-    sha256 = "sha256-/pn2rVWM+e7PYBMddcVNb46lEmyyac1vMmXAz4AohfE=";
+    sha256 = "sha256-gbiH0TdFsodvlsHjX4v3bSPrDR2nIQ3OLCPTxW59WFQ=";
   };
 in
 buildDmgAppImpure google-chrome "Google Chrome" {
   pname = "google-chrome";
-  version = "latest";
+  version = "124.0.6367.118";
 
   meta = with lib; {
     description = "The official web browser from Google";
