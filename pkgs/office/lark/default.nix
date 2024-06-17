1c9ebbe355d994bb30324c49880fc4050d0b4e01
tsssni <dingyongyu2002@foxmail.com>
2024-06-17 13:21:20 +0800

    update darwin-fonts & google-chrome & lark

--- pkgs/office/lark/default.nix
+++ pkgs/office/lark/default.nix
@@ -4,10 +4,15 @@
 }:
 let
   pname = "lark";
-  version = "7.18.8";
-  lark = fetchurl {
-    url = "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/58c0089d/Lark-darwin_arm64-${version}-signed.dmg";
-    sha256 = "sha256-vkork0IeVZF7IVsxRpj/t4UUIKUCIrnolickwL7npcI=";
+  version = "7.19.6,75799bdd";
+
+  lark = let
+    versionSplit = lib.strings.splitString "," version;
+    mainVersion = builtins.elemAt versionSplit 0;
+    patch = builtins.elemAt versionSplit 1;
+  in fetchurl {
+    url = "https://sf16-va.larksuitecdn.com/obj/lark-artifact-storage/${patch}/Lark-darwin_arm64-${mainVersion}-signed.dmg";
+    sha256 = "sha256-cUxTT1cAKSrQswL9gUQC8E3Q0vlMOo5PMMCt5lJVJ5Q=";
   };
 in
 buildDmgApp lark {
--- pkgs/wired/google-chrome/default.nix
+++ pkgs/wired/google-chrome/default.nix
@@ -5,12 +5,12 @@
 let
   google-chrome = fetchurl {
     url = "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg";
-    sha256 = "sha256-8OlzATGTC8t/dflmTFW2iU3rFgZQFP9xo2DYDQVlUFc=";
+    sha256 = "sha256-yXNycVHIlXntkP020M/hxa/FY5VIo0pqHf17RIRooWs=";
   };
 in
 buildDmgAppImpure google-chrome "Google Chrome" {
   pname = "google-chrome";
-  version = "125.0.6422.142";
+  version = "126.0.6478.62";
 
   meta = with lib; {
     description = "The official web browser from Google";
